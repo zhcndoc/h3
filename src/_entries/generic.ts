ab09098e411bd852845d201ee4eff15aa80962ba
Pooya Parsa <pyapar@gmail.com>
2025-07-02 11:13:04 +0200

    feat: freeze app routes and config after server init (#1140)

--- src/_entries/generic.ts
+++ src/_entries/generic.ts
@@ -1,6 +1,7 @@
 import type { ServerOptions, Server } from "srvx";
 import type { H3 } from "../h3.ts";
 import { serve as srvxServe } from "srvx/generic";
+import { freezeApp } from "./_common.ts";
 
 // Main exports
 export * from "../index.ts";
@@ -9,5 +10,6 @@ export * from "../index.ts";
  * Serve the H3 app.
  */
 export function serve(app: H3, options?: Omit<ServerOptions, "fetch">): Server {
+  freezeApp(app);
   return srvxServe({ fetch: app._fetch, ...options });
 }
