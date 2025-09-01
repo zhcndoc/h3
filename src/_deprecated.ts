e530e717098862697003c0343a1d6adc17cc36af
abeer0 <47961062+iiio2@users.noreply.github.com>
2025-09-01 21:49:57 +0600

    chore: import `ProxyOptions` separately (#1199)

--- src/_deprecated.ts
+++ src/_deprecated.ts
@@ -5,11 +5,12 @@ import {
   toNodeHandler,
 } from "./adapters.ts";
 import { defineHandler, defineLazyEventHandler } from "./handler.ts";
-import { proxy, type ProxyOptions } from "./utils/proxy.ts";
+import { proxy } from "./utils/proxy.ts";
 import { H3 } from "./h3.ts";
 import { withBase } from "./utils/base.ts";
 import { sanitizeStatusCode, sanitizeStatusMessage } from "./utils/sanitize.ts";
 
+import type { ProxyOptions } from "./utils/proxy.ts";
 import type { NodeHandler, NodeMiddleware } from "./adapters.ts";
 import type { H3Event } from "./event.ts";
 import type { EventHandler } from "./types/handler.ts";
